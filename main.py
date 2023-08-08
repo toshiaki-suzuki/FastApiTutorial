c9fa06503fa7b22662eec7d957c4f6ead26bf050
toshiaki-suzuki <saki0x4bf@gmail.com>
2023-08-08 19:50:10 +0900

    クエリパラメータを追加

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ class ModelName(str, Enum):
 
 app = FastAPI()
 
+fake_items_db = [
+    {"item_name": "Foo"},
+    {"item_name": "Bar"},
+    {"item_name": "Baz"}
+]
+
 
 @app.get("/")
 def root():
@@ -45,3 +51,23 @@ async def get_model(model_name: ModelName):
 @app.get("/files/{file_path:path}")  # ファイルパスを含んだパスパラメータ
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+
+# クエリパラメータ
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
