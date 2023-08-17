b23476dd4d072e9f7e7bf16828faa69d1681b0d6
toshiaki-suzuki <saki0x4bf@gmail.com>
2023-08-17 19:39:36 +0900

    タスク取得APIのテストを作成

--- test/test_main.py
+++ test/test_main.py
@@ -21,3 +21,17 @@ def test_read_tasks_200():
     response = client.get("/tasks")
     assert response.status_code == 200
     assert response.json() == {"tasks": tasks}
+
+
+def test_read_task_200():
+
+    response = client.get("/tasks/1")
+    assert response.status_code == 200
+    assert response.json() == {"task": tasks[0]}
+
+
+def test_read_task_404():
+
+    response = client.get("/tasks/0")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Task not found"}
