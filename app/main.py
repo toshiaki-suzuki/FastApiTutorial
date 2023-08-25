cb47c2f1ab36ff065499082263fbf26e9a4a07e7
toshiaki-suzuki <saki0x4bf@gmail.com>
2023-08-25 09:06:39 +0900

    DB接続

--- app/main.py
+++ app/main.py
@@ -1,6 +1,40 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
+from sqlalchemy import create_engine, Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+load_dotenv()
+
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("DB_NAME")
+
+DATABASE_URL = f"mysql+mysqldb://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), index=True)
+    status = Column(String(50))
+
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup_event():
+    Base.metadata.create_all(bind=engine)
 
 tasks = [
     {
