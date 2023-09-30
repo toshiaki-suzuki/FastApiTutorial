a1577f08f779ccbc1df575a8f8d4d2f325e8c88e
toshiaki-suzuki <saki0x4bf@gmail.com>
2023-09-30 16:44:02 +0900

    不要コード削除

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
